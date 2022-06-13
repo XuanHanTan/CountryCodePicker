d60d4384df416c975c3a47cbdb966b31bb405871
Xuan Han Tan <43411204+XuanHanTan@users.noreply.github.com>
2022-06-13 16:37:49 +0800

    Fixed weird text alignment

--- lib/selection_dialog.dart
+++ lib/selection_dialog.dart
@@ -112,7 +112,7 @@ class _SelectionDialogState extends State<SelectionDialog> {
                     child: Padding(
                       padding: const EdgeInsets.symmetric(vertical: 10),
                       child: TextField(
-                        textAlignVertical: TextAlignVertical.center,
+                        cursorHeight: widget.searchStyle?.fontSize,
                         maxLines: 1,
                         style: widget.searchStyle,
                         controller: searchTextFieldController,
